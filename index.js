1b6483fbb39711807e0389a07d3bf3d4e5443094
Rxy191 <raxxyaja@gmail.com>
2025-08-30 03:02:49 +0700

    update index.js

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ const client = new Client({
 
 client.once("ready", () => {
   console.log(`✅ Bot ${client.user.tag} udah online!`);
+  // contoh: kirim pesan ke channel pas bot nyala
+  const channelId = "1410904568857890902"; 
+  const channel = client.channels.cache.get(channelId);
+  if (channel) channel.send("🤖 Bot udah online dan siap kerja!");
 });
 
-// contoh command di Discord
 client.on("messageCreate", (message) => {
-  if (message.content === "!statusbot") {
+  if (message.content === "!ping") {
     message.channel.send("🏓 Pong!");
   }
 });
 
-// login pakai token dari Render
 client.login(process.env.DISCORD_TOKEN);
